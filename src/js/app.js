d4524711760d1459ea3332f7d7fd099340490b23
Maks <maksym.oleksyuk@gmail.com>
2021-11-08 17:41:45 +0200

    08.11

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,12 @@
 $(document).on("scroll", function () {
-  var scrollTop = $(window).scrollTop();
+  let scrollTop = $(window).scrollTop();
+  let h = $("header").height();
   if (scrollTop >= 19) {
     $("header").addClass("navbar-fix");
+    $(".home .container:nth-child(2)").css({ "padding-top": h + 40 + "px" });
   } else {
     $("header").removeClass("navbar-fix");
+    $(".home .container:nth-child(2)").css({ "padding-top": "40px" });
   }
+  $("body").attr("data-bs-offset", h + 15);
 });
